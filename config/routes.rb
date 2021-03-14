19607e309514f064c7f1561b92c85f9f01216e0d
Ayumu-O <ayumu.osawa0904@icloud.com>
2021-03-14 20:58:24 +0900

    edit home_page #2

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "signup", :to => "users/registrations#new"
+    get "user/:id", :to => "users/registrations#detail"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
