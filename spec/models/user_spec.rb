2533bfaa3f4ba994911149d832e72bfdc275c558
Ayumu-O <ayumu.osawa0904@icloud.com>
2021-03-16 22:59:44 +0900

    modify user_spec #8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is invalid" do
-    user = User.new
-    expect(user).not_to be_valid
+  ii "is valid with a name, email, password" do
+    user = User.new(
+      name: "チュートリアル　太郎"
+      email: "test@test.com"
+      password: "password"
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without name" do
+    user.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without email" do
+    user.new(email: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without password" do
+    user.new(password: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email" do
+    User.create(
+      name: "チュートリアル　太郎"
+      email: "test2@test.com"
+      password: "password"
+    )
+
+    user = User.new(
+      name: "チュートリアル　次郎"
+      email: "test2@test.com"
+      password: "password"
+    )
+
+    expect(user.errors[:email]).to include("has already been taken")
   end
 end
