823ac034258d3284446235d5196520db58771ba7
Ayumu-O <ayumu.osawa0904@icloud.com>
2021-03-16 23:33:45 +0900

    modify rails_helper to use factory_bot #8

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,31 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = build(:user)
+  end
+
   ii "is valid with a name, email, password" do
-    user = User.new(
-      name: "チュートリアル　太郎"
-      email: "test@test.com"
-      password: "password"
-    )
-    expect(user).to be_valid
+    expect(@user).to be_valid
   end
 
   it "is invalid without name" do
-    user.new(name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
+    @user.name = nil
+    @user.valid?
+    expect(@user.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without email" do
-    user.new(email: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
+    @user.email = nil
+    @user.valid?
+    expect(@user.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid without password" do
-    user.new(password: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
+    @user.password = nil
+    @user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate email" do
